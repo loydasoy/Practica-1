55bee463aec43d5c672a2a2817c998e44b441b0c
Tu <you@exampl.com>
2025-12-05 12:53:17 +0100

    ejercicio 3 con error

---
+++ Ejercicio1_UD3.java
@@ -0,0 +1,20 @@
+public class Ejercicio1_UD3 {
+    static int proceso1(int n1)
+    { int p1 = 1;
+        for(int i = 0; i < n1; i++) p1 += proceso2(i);
+        return p1;
+}
+static int proceso2(int n2)
+{
+    int p2 = 1;
+    for(int i = 0; i < n2; i++) p2 += n2;
+    return p2;
+    }
+public static void main (String args[])
+{
+    int num = 3;
+    int res = proceso1(num);
+    }
+}
+
+
